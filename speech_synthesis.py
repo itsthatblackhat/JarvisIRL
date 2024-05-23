cf88296b30185aee6bfa3ccb72e3b3230d00b899
blackhat <1538658+itsthatblackhat@users.noreply.github.com>
2024-05-22 23:16:10 -0500

    Delete speech_synthesis.py

--- speech_synthesis.py
+++
@@ -1,9 +0,0 @@
-import pyttsx3
-
-class SpeechSynthesis:
-    def __init__(self):
-        self.engine = pyttsx3.init()
-
-    def speak(self, text):
-        self.engine.say(text)
-        self.engine.runAndWait()
